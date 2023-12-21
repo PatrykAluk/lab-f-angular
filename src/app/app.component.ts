4ae84b23902d04e93f522e86e3059c9b4e7a9a35
PatrykAluk <ap50319@zut.edu.pl>
2023-12-21 16:12:55 +0100

    lab-f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'lab-f';
+  title: string = 'lab-f';
+
+  constructor(
+    private authService: AuthService,
+  ) { }
+
+  public canAccessUsers(): boolean {
+    return this.authService.isAdmin();
+  }
+
+  public canAccessItems(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  getUsername() {
+    return this.authService.getUsername();
+  }
+
+  public logout(): void {
+    this.authService.logout();
+  }
 }
